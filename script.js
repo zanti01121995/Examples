f60e7e34a1588add06fa2be39c8d4349d9607a3b
Zanti01121995 <santhoshsekar788@gmail.com>
2023-04-08 11:37:20 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+
+
+// var xrh = new XMLHttpRequest();
+// xrh.open('GET','http://ergast.com/api/f1/2004/1/results.json');
+// xml.onload = function(){
+//     console.log(xrh.response)
+// }
+// xrh.send();
